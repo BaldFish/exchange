8156274f60e066914784a4a52526e9b726946b8b
lwc <wence.lv@cnlaunch.com>
2018-05-23 18:19:09 +0800

    首页布局开发

--- src/App.vue
+++ src/App.vue
@@ -12,29 +12,29 @@
     </div>
     <div class="footer-wrap">
       <div class="footer">
-        <div class="fl-box">
-          <img src="" alt="log">
+        <div class="ft-box">
+          <img class="ft_logo" src="./common/images/logo_footer.png" alt="log">
         </div>
-        <div class="fl-box">
-          <p>资产服务</p>
-          <ul>
+        <div class="ft-box">
+          <span>资产服务</span>
+          <ul class="text">
             <li><a href="javascript:void(0)">维修案例</a></li>
             <li><a href="javascript:void(0)">维修设备</a></li>
             <li><a href="javascript:void(0)">汽车资产</a></li>
             <li><a href="javascript:void(0)">汽车零部件</a></li>
           </ul>
         </div>
-        <div class="fl-box">
-          <ul>
+        <div class="ft-box">
+          <ul class="code">
             <li>
-              <a href="#/product">
-                <img src="" alt="Android">
+              <a href="javascript:void(0)">
+                <img class="ft_code" src="./common/images/Android.png" alt="Android">
                 <p>Android版</p>
               </a>
             </li>
             <li>
-              <a href="#/product">
-                <img src="" alt="IOS">
+              <a href="javascript:void(0)">
+                <img class="ft_code" src="./common/images/IOS.png" alt="IOS">
                 <p>IOS版</p>
               </a>
             </li>
@@ -47,9 +47,10 @@
 
 <script>
   import "./common/stylus/index.styl";
-export default {
-  name: 'App'
-}
+  
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style lang="stylus">
@@ -61,15 +62,16 @@ export default {
   
   .head-wrap {
     width: 100%;
+    min-width 1212px
     height: 34px;
     background-color: #e5e5e5;
     .head {
       box-sizing: border-box
-      width: 1200px;
+      width: 1212px;
       margin: 0 auto;
       text-align right
       line-height 34px
-      a{
+      a {
         margin-left 30px
         color: #666666;
       }
@@ -81,32 +83,67 @@ export default {
     box-sizing: border-box;
     margin: 0 auto;
     width 100%
-    min-width 1200px
+    min-width 1212px
   }
   .footer-wrap {
     width 100%
-    min-width 1200px
+    min-width 1212px
     background-color: #272324;
     .footer {
       box-sizing: border-box
-      width 1200px
+      width 1212px
       height 156px
       margin 0 auto
-      .fl-box {
-        p{
-          font-size 16px
-          color #ffffff
+      font-size 0
+      text-align center
+      .ft-box {
+        display inline-block
+        font-size 16px
+        vertical-align top
+        margin-right 66px
+        .ft_logo {
+          margin-top 38px
         }
-        ul{
+        span {
+          padding-top 45px
+          display block
+          color #ffffff
+          text-align left
+        }
+        .text {
           font-size 0
-          li{
+          li {
+            padding-top 25px
             display inline-block
             font-size 14px
-            a{
+            margin-right 77px
+            a {
               color #ffffff
             }
           }
+          li:last-child {
+            margin-right 0
+          }
         }
+        .code {
+          font-size 0
+          li {
+            padding 30px 16px 0
+            display inline-block
+            font-size 14px
+            a {
+              color #ffffff
+              p {
+                font-size 16px
+                color #ffffff
+                padding-top 14px
+              }
+            }
+          }
+        }
+      }
+      .ft-box:last-child {
+        margin-right -16px
       }
     }
   }
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home">
-    home
+    <div class="top">top</div>
+    <div class="banner"></div>
+    <div class="list">
+      <div class="case clearfix">
+        <div class="fl fl_bg"></div>
+        <div class="fr">1</div>
+      </div>
+      <div class="facility clearfix">
+        <div class="fl fl_bg"></div>
+        <div class="fr">2</div>
+      </div>
+      <!--<div class="property clearfix">
+        <div class="fl fl_bg"></div>
+        <div class="fr">3</div>
+      </div>
+      <div class="parts clearfix">
+        <div class="fl fl_bg"></div>
+        <div class="fr">4</div>
+      </div>-->
+      <div class="partner clearfix">
+        <div class="fl fl_bg"></div>
+        <div class="fr">5</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,14 +31,78 @@
   export default {
     name: "home",
     data() {
-      return {
-      }
+      return {}
     },
   }
 </script>
 
 <style scoped lang="stylus">
-  .home{
-  
+  .home {
+    .top {
+      box-sizing border-box
+      width 1212px
+      height 130px
+      margin 0 auto
+      border 1px solid red
+    }
+    .banner {
+      box-sizing border-box
+      height 600px
+      background-image: url('./images/banner.png');
+      background-position: top center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      /*background-size:contain;*/
+      /*background-size:auto;*/
+    }
+    .list {
+      width 1212px
+      margin 0 auto
+      .case{
+        .fl_bg{
+          width 346px
+          height 734px
+          background-image: url('./images/01.png');
+          background-position: top center;
+          background-repeat: no-repeat;
+        }
+      }
+      .facility{
+        .fl_bg{
+          width 346px
+          height 734px
+          background-image: url('./images/02.png');
+          background-position: top center;
+          background-repeat: no-repeat;
+        }
+      }
+      /*.property{
+        .fl_bg{
+          width 346px
+          height 734px
+          background-image: url('./images/03.png');
+          background-position: top center;
+          background-repeat: no-repeat;
+        }
+      }
+      .parts{
+        .fl_bg{
+          width 346px
+          height 734px
+          background-image: url('./images/04.png');
+          background-position: top center;
+          background-repeat: no-repeat;
+        }
+      }*/
+      .partner{
+        .fl_bg{
+          width 346px
+          height 330px
+          background-image: url('./images/cooperation.png');
+          background-position: top right ;
+          background-repeat: no-repeat;
+        }
+      }
+    }
   }
 </style>
