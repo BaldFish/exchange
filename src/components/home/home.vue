4f665a82a827c364706f25c26050c693683f913a
lwc <wence.lv@cnlaunch.com>
2018-05-24 18:01:50 +0800

    除top部分首页完成

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -11,20 +11,63 @@
         <div class="fr fr_case">
           <h4><a href="javascript:void(0)">奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</a></h4>
           <div class="attestation">
-            <span>认证个人</span>
-            <span>高可信</span>
+            <span class="person">认证个人</span>
+            <span class="trust">高可信</span>
           </div>
-          <div class="time">
-            <a href="javascript:void(0)">上架时间：2018.03.23 21:22:24</a>
-            <a href="javascript:void(0)">权益：所有权</a>
+          <div class="putaway">
+            <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+            <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
           </div>
           <div class="fault">
             <p>
-              故障现象：客户反映，正常行驶中，等红绿灯时，发现车辆抖动并且越来越严重，随后发动机故障灯亮起，靠边停车熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，
-              动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓 解常亮.......
+              <span>故障现象：</span>客户反映，正常行驶中，等红绿灯时，发现车辆抖动并且越来越严重，随后发动机故障灯亮起，靠边停车熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解常亮隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解常亮
             </p>
           </div>
-          <div></div>
+          <div class="price_box">
+            <a href=""><p class="price">139.00</p></a>
+            <a href=""><p class="tracing">可信溯源</p></a>
+          </div>
+        </div>
+        <div class="fr fr_case">
+          <h4><a href="javascript:void(0)">奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</a></h4>
+          <div class="attestation">
+            <span class="merchant">认证商家</span>
+            <span class="trust">高可信</span>
+          </div>
+          <div class="putaway">
+            <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+            <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+          </div>
+          <div class="fault">
+            <p>
+              <span>故障现象：</span>客户反映，正常行驶中，等红绿灯时，发现车辆抖动并且越来越严重，随后发动机故障灯亮起，靠边停车熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解常亮隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解常亮
+            </p>
+          </div>
+          <div class="price_box">
+            <a href=""><p class="price">139.00</p></a>
+            <a href=""><p class="tracing">可信溯源</p></a>
+          </div>
+        </div>
+        <div class="fr fr_case">
+          <h4><a href="javascript:void(0)">奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</a></h4>
+          <div class="attestation">
+            <span class="person">认证个人</span>
+            <span class="trust">高可信</span>
+          </div>
+          <div class="putaway">
+            <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+            <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+          </div>
+          <div class="fault">
+            <p>
+              <a href="javascript:void(0)"><span>故障现象：</span>客户反映，正常行驶中，等红绿灯时，发现车辆抖动并且越来越严重，随后发动机故障灯亮起，靠边停车熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解常亮隔15min重新启动车辆，抖动稍缓解熄火，隔15min重新启动车辆，抖动稍缓解，但发动但发动机有很大很很大杂音，动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解动稍缓解常亮</a>
+            
+            </p>
+          </div>
+          <div class="price_box">
+            <a href=""><p class="price">139.00</p></a>
+            <a href=""><p class="tracing">可信溯源</p></a>
+          </div>
         </div>
       </div>
       <div class="facility clearfix">
@@ -32,7 +75,116 @@
           <h3>维修设备</h3>
           <a href="javascript:void(0)">查看更多</a>
         </div>
-        <div class="fr">2</div>
+        <div class="fr fr_facility">
+          <div class="fl facility_info">
+            <a href="javascript:void(0)"><h4>奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</h4></a>
+            <div class="putaway">
+              <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+              <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+            </div>
+            <div class="fl price_box">
+              <a href="javascript:void(0)"><p class="price">139.00</p></a>
+              <a href="javascript:void(0)"><p class="tracing">可信溯源</p></a>
+            </div>
+            <div class="fr facility_img">
+              <a href="javascript:void(0)"><img src="./images/05.png" alt=""></a>
+            </div>
+            <div class="attestation clearfix">
+              <span class="merchant">认证商家</span>
+              <span class="trust">低可信</span>
+            </div>
+          </div>
+          <div class="fl facility_info">
+            <a href="javascript:void(0)"><h4>奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</h4></a>
+            <div class="putaway">
+              <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+              <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+            </div>
+            <div class="fl price_box">
+              <a href="javascript:void(0)"><p class="price">139.00</p></a>
+              <a href="javascript:void(0)"><p class="tracing">可信溯源</p></a>
+            </div>
+            <div class="fr facility_img">
+              <a href="javascript:void(0)"><img src="./images/05.png" alt=""></a>
+            </div>
+            <div class="attestation clearfix">
+              <span class="merchant">认证商家</span>
+              <span class="trust">低可信</span>
+            </div>
+          </div>
+          <div class="fl facility_info">
+            <a href="javascript:void(0)"><h4>奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</h4></a>
+            <div class="putaway">
+              <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+              <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+            </div>
+            <div class="fl price_box">
+              <a href="javascript:void(0)"><p class="price">139.00</p></a>
+              <a href="javascript:void(0)"><p class="tracing">可信溯源</p></a>
+            </div>
+            <div class="fr facility_img">
+              <a href="javascript:void(0)"><img src="./images/05.png" alt=""></a>
+            </div>
+            <div class="attestation clearfix">
+              <span class="merchant">认证商家</span>
+              <span class="trust">低可信</span>
+            </div>
+          </div>
+          <div class="fl facility_info">
+            <a href="javascript:void(0)"><h4>奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</h4></a>
+            <div class="putaway">
+              <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+              <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+            </div>
+            <div class="fl price_box">
+              <a href="javascript:void(0)"><p class="price">139.00</p></a>
+              <a href="javascript:void(0)"><p class="tracing">可信溯源</p></a>
+            </div>
+            <div class="fr facility_img">
+              <a href="javascript:void(0)"><img src="./images/05.png" alt=""></a>
+            </div>
+            <div class="attestation clearfix">
+              <span class="merchant">认证商家</span>
+              <span class="trust">低可信</span>
+            </div>
+          </div>
+          <div class="fl facility_info">
+            <a href="javascript:void(0)"><h4>奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</h4></a>
+            <div class="putaway">
+              <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+              <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+            </div>
+            <div class="fl price_box">
+              <a href="javascript:void(0)"><p class="price">139.00</p></a>
+              <a href="javascript:void(0)"><p class="tracing">可信溯源</p></a>
+            </div>
+            <div class="fr facility_img">
+              <a href="javascript:void(0)"><img src="./images/05.png" alt=""></a>
+            </div>
+            <div class="attestation clearfix">
+              <span class="merchant">认证商家</span>
+              <span class="trust">低可信</span>
+            </div>
+          </div>
+          <div class="fl facility_info">
+            <a href="javascript:void(0)"><h4>奔驰E20456785676540发动机怠速时抖动严重发动机严重动严严重发动重发动机有音可动机有音可</h4></a>
+            <div class="putaway">
+              <a class="time" href="javascript:void(0)"><span>上架时间：</span>2018.03.23 21:22:24</a>
+              <a class="equity" href="javascript:void(0)"><span>权益：</span>所有权</a>
+            </div>
+            <div class="fl price_box">
+              <a href="javascript:void(0)"><p class="price">139.00</p></a>
+              <a href="javascript:void(0)"><p class="tracing">可信溯源</p></a>
+            </div>
+            <div class="fr facility_img">
+              <a href="javascript:void(0)"><img src="./images/05.png" alt=""></a>
+            </div>
+            <div class="attestation clearfix">
+              <span class="merchant">认证商家</span>
+              <span class="trust">低可信</span>
+            </div>
+          </div>
+        </div>
       </div>
       <!--<div class="property clearfix">
         <div class="fl fl_bg"></div>
@@ -46,7 +198,26 @@
         <div class="fl fl_bg">
           <h3>合作伙伴</h3>
         </div>
-        <div class="fr">5</div>
+        <div class="fr fr_partner">
+          <div class="fl partner_img">
+            <img src="./images/06.png" alt="">
+          </div>
+          <div class="fl partner_img">
+            <img src="./images/06.png" alt="">
+          </div>
+          <div class="fl partner_img">
+            <img src="./images/06.png" alt="">
+          </div>
+          <div class="fl partner_img">
+            <img src="./images/07.png" alt="">
+          </div>
+          <div class="fl partner_img">
+            <img src="./images/07.png" alt="">
+          </div>
+          <div class="fl partner_img">
+            <img src="./images/07.png" alt="">
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -84,7 +255,7 @@
       width 1212px
       margin 0 auto
       margin-top 32px
-      padding-bottom 100px
+      padding-bottom 88px
       .fl_bg {
         width 346px
         background-position: top center;
@@ -137,30 +308,262 @@
         .fl_bg {
           height 330px
           background-image: url('./images/cooperation.png');
-          h3{
+          background-position: top left 16px ;
+          h3 {
             margin-top 132px
           }
         }
       }
       .fr_case {
+        margin-bottom 18px
+        position relative
         box-sizing border-box
         width 854px
         height 220px
         background-color rgba(255, 255, 255, 0.8)
         opacity: 0.8;
         padding 15px 20px
-        h4{
+        h4 {
           overflow: hidden;
-          text-overflow:ellipsis;
+          text-overflow: ellipsis;
           white-space: nowrap;
-          a{
+          a {
             color: #222222;
             font-size: 20px;
           }
         }
-        .attestation{}
-        .time{}    
-        .fault{}
+        .attestation {
+          font-size 0px
+          span {
+            text-align center
+            color #ffffff
+            font-size 14px
+            display inline-block
+            width 70px
+            height 30px
+            line-height 30px
+            margin-right 14px
+            margin-top 14px
+            margin-bottom 30px
+          }
+          .trust {
+            background-color #99c7ff
+          }
+          .person {
+            background-color #ffdd99
+          }
+          .merchant {
+            background-color #ffa799
+          }
+        }
+        .putaway {
+          a {
+            padding-left 26px
+            padding-top 2px
+            padding-bottom 2px
+            color #666666;
+            font-size 14px
+            margin-right 43px
+            background-repeat: no-repeat;
+            background-position: top left;
+            span {
+              color #222222;
+              font-size 16px
+            }
+          }
+          .time {
+            background-image: url('./images/time.png');
+          }
+          .equity {
+            background-image: url('./images/Profit.png');
+          }
+        }
+        .fault {
+          p {
+            padding-top 10px
+            width 562px
+            height 66px
+            line-height 16px
+            a {
+              color #666666;
+              font-size 14px
+              span {
+                font-size 16px
+                color #222222;
+              }
+            }
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 4;
+            overflow: hidden;
+          }
+        }
+        .price_box {
+          position absolute
+          top 78px
+          right 84px
+          .price {
+            a {
+              display block
+            }
+            font-size: 24px;
+            color: #c6351e;
+            background-image: url('./images/currency.png');
+            background-repeat: no-repeat;
+            background-position: top left;
+            padding-left 26px
+            margin-bottom 10px
+          }
+          .tracing {
+            color: #f3b43f;
+            font-size 14px
+            background-image: url('./images/credible.png');
+            background-repeat: no-repeat;
+            background-position: top left;
+            padding-left 26px
+            height 18px
+            line-height 18px
+          }
+        }
+      }
+      .fr_facility {
+        width 854px
+        .facility_info {
+          margin-bottom 20px
+          margin-right 22px
+          box-sizing border-box
+          width 270px
+          height 340px
+          padding 22px 15px 0
+          background-color #ffffff
+          h4 {
+            color: #222222;
+            font-size: 18px;
+            height 40px
+            line-height 20px
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+          }
+          .putaway {
+            margin-top 32px
+            a {
+              display block
+              padding-left 26px
+              padding-top 2px
+              padding-bottom 2px
+              color #666666;
+              font-size 14px
+              background-repeat: no-repeat;
+              background-position: top left;
+              span {
+                color #222222;
+                font-size 16px
+              }
+            }
+            .time {
+              background-image: url('./images/time.png');
+              padding-bottom 12px
+            }
+            .equity {
+              background-image: url('./images/Profit.png');
+            }
+          }
+          .price_box {
+            margin-top 43px
+            .price {
+              a {
+                display block
+              }
+              font-size: 24px;
+              color: #c6351e;
+              background-image: url('./images/currency.png');
+              background-repeat: no-repeat;
+              background-position: top left;
+              padding-left 26px
+              margin-bottom 10px
+            }
+            .tracing {
+              color: #f3b43f;
+              font-size 14px
+              background-image: url('./images/credible.png');
+              background-repeat: no-repeat;
+              background-position: top left;
+              padding-left 26px
+              height 18px
+              line-height 18px
+            }
+          }
+          .facility_img {
+            display: table-cell;
+            vertical-align: middle;
+            text-align: center;
+            img {
+              vertical-align: middle;
+              position: relative;
+              top: 50%;
+              transform: translateY(-50%);
+            }
+            a {
+              display block
+              width 92px
+              height 92px
+              border 1px solid #eeeeee
+            }
+          }
+          .attestation {
+            font-size 0px
+            span {
+              text-align center
+              color #ffffff
+              font-size 14px
+              display inline-block
+              width 70px
+              height 30px
+              line-height 30px
+              margin-right 14px
+              margin-top 30px
+              margin-bottom 30px
+            }
+            .trust {
+              background-color #99c7ff
+            }
+            .person {
+              background-color #ffdd99
+            }
+            .merchant {
+              background-color #ffa799
+            }
+          }
+        }
+        .facility_info:nth-child(3n) {
+          margin-right 0
+        }
+      }
+      .fr_partner {
+        width 854px
+        .partner_img {
+          box-sizing border-box
+          background-color: #ffffff;
+          margin-bottom 14px
+          margin-right 12px
+          display: table-cell;
+          width: 276px;
+          height: 158px;
+          vertical-align: middle;
+          text-align: center;
+          img {
+            vertical-align: middle;
+            position: relative;
+            top: 50%;
+            transform: translateY(-50%);
+          }
+          
+        }
+        .partner_img:nth-child(3n) {
+          margin-right 0
+        }
       }
     }
   }
