5f8d025b0e608fb771150687295b0d7c96175d04
lwc <wence.lv@cnlaunch.com>
2018-10-18 15:03:15 +0800

    更改国内分支domain

--- src/App.vue
+++ src/App.vue
@@ -261,8 +261,8 @@
           sessionStorage.removeItem('userInfo');
           /*document.cookie = `token=;expires=${new Date(0)}`;
           document.cookie = `user_id=;expires=${new Date(0)}`;*/
-          document.cookie = `token=;expires=${new Date(0)};domain=.launchain.org`;
-          document.cookie = `user_id=;expires=${new Date(0)};domain=.launchain.org`;
+          document.cookie = `token=;expires=${new Date(0)};domain=.launchain.cn`;
+          document.cookie = `user_id=;expires=${new Date(0)};domain=.launchain.cn`;
           this.switchover = false;
           location.reload()
         }).catch(error => {
