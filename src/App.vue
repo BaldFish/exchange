632f3eab1a74e01b215afb2557241e0206f6bacc
lwc <wence.lv@cnlaunch.com>
2019-04-22 16:22:44 +0800

    与转让平台调试

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <div class="head">
         <a class="logo" href="/">
           <img src="./common/images/logo.png" alt="">
-          <!--<p>Trusted Assets Blockchain</p>-->
         </a>
         <ul class="platform">
           <li v-for="(item,index) of toggleParam" @click="platform(index)" :class="{active:index===toggleIndex}">{{item}}</li>
@@ -17,7 +16,6 @@
         
         <div class="no_login" v-if="!isLogin">
           <a href="javascript:void(0)" @click="login">登录/注册</a>
-          <!--<a href="javascript:void(0)" @click="register">免费注册</a>-->
         </div>
         <div class="login" v-if="isLogin" @mouseleave.stop="leaveUl">
           <div @click.stop="toggle">{{userName}} <img src="./common/images/down.png" alt=""></div>
@@ -29,35 +27,6 @@
         </div>
       </div>
     </div>
-    <!--<div class="login-header" v-if="isShowLogin">
-      <div class="login-header-cont">
-        <router-link to="/home">
-          <img src="./common/images/login_header.png" alt="">
-        </router-link>
-      </div>
-    </div>
-    <div class="forget_psw_header" v-if="isShowRegister">
-      <section>
-        <router-link to="/home">
-          <img src="./common/images/register_logo.png" alt="">
-        </router-link>
-        <p>已有账号，立即
-          <router-link to="/login" class="to_login">登录</router-link>
-        </p>
-      </section>
-    </div>
-    <div class="forget_psw_header" v-if="isShowForgetPassword">
-      <section>
-        <router-link to="/home">
-          <img src="./common/images/forget_psw_logo.png" alt="">
-        </router-link>
-        <p>已有账号，立即
-          <router-link to="/login" class="to_login">登录</router-link>
-        </p>
-      </section>
-    </div>-->
-    <!--<my-topSearch v-if="isShowTopSearch"></my-topSearch>
-    <my-toggle :toggleIndex="toggleIndex"></my-toggle>-->
     <div class="main_wrap">
       <router-view class="main" v-if="isRouterAlive"></router-view>
     </div>
@@ -171,7 +140,7 @@
         isShowRegister: false,
         isShowForgetPassword: false,
         toggleIndex: 1,
-        toggleParam: ["搜索", "交易平台", "开发者计划"],
+        toggleParam: ["搜索", "交易平台", "转让平台","开发者计划"],
         userId: '',
         token: "",
       }
@@ -201,7 +170,6 @@
             this.isLogin = false;
             sessionStorage.removeItem('loginInfo');
             sessionStorage.removeItem('userInfo');
-            //this.dropOut()
           }
         }).catch((err) => {
           console.log(err);
@@ -210,7 +178,6 @@
         sessionStorage.removeItem('loginInfo');
         sessionStorage.removeItem('userInfo');
       }
-      //this.changTop()
     },
     mounted() {
       if(this.pathname==="/developer"){
@@ -253,7 +220,6 @@
         sessionStorage.removeItem('loginInfo');
         sessionStorage.removeItem('userInfo');
       }
-      //this.changTop()
     },
     computed: {
       pathname:{
@@ -349,34 +315,6 @@
         let url=`?redirectURL=${redirectURL}`;
         window.location.href=`${loginPlatform}/register${url}`;
       },
-      /*changTop() {
-        if (this.$route.path == "/login") {
-          this.isShowTopSearch = false;
-          this.isShowLogin = true;
-          this.isShowRegister = false;
-          this.isShowForgetPassword = false;
-        } else if (this.$route.path == "/register") {
-          this.isShowTopSearch = false;
-          this.isShowLogin = false;
-          this.isShowRegister = true;
-          this.isShowForgetPassword = false;
-        } else if (this.$route.path == "/forgetPassword") {
-          this.isShowTopSearch = false;
-          this.isShowLogin = false;
-          this.isShowRegister = false;
-          this.isShowForgetPassword = true;
-        } else if (this.$route.path == "/contract") {
-          this.isShowTopSearch = false;
-          this.isShowLogin = false;
-          this.isShowRegister = false;
-          this.isShowForgetPassword = false;
-        }else {
-          this.isShowTopSearch = true;
-          this.isShowLogin = false;
-          this.isShowRegister = false;
-          this.isShowForgetPassword = false;
-        }
-      },*/
       reload() {
         this.isRouterAlive = false;
         this.$nextTick(() => {
@@ -416,7 +354,9 @@
         } else if (index === 1) {
           window.location.href = exchangePlatform
         } else if (index === 2) {
-          window.location.href = "/developer"
+          window.location.href = transferPlatform
+        }else if (index === 3) {
+          this.$router.push("/developer");
         }
       },
       open() {
@@ -617,43 +557,6 @@
     }
   }
   
-  /*.login-header {
-    width: 100%;
-    height: 130px;
-    background-color: #f3f3f3;
-    .login-header-cont {
-      width: 1200px;
-      margin: 0 auto;
-      height: 100%;
-      padding-top 36px
-      img {
-        width: 280px;
-        height: 58px;
-        display: inline-block;
-      }
-    }
-  }
-  
-  .forget_psw_header {
-    width: 100%;
-    height: 130px;
-    background-color: #f3f3f3;
-    section {
-      width: 1200px;
-      margin: 0 auto;
-      padding-top: 36px;
-      p {
-        float: right;
-        margin-top: 36px;
-        font-size: 16px;
-        color: #222222;
-        .to_login {
-          color: #c6351e;
-        }
-      }
-    }
-  }
-  */
   .main_wrap {
     flex: 1;
     box-sizing: border-box;
